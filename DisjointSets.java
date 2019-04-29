37efe2cc182b80e4201c44daa22cdf6384d9905f
CS-Queens-College-Yao <carolyn.yao@qc.cuny.edu>
2019-04-29 17:53:37 -0400

    first commit

---
+++ DisjointSets.java
@@ -0,0 +1,47 @@
+/**
+ * You are trying to keep track of friend circles. Remember the basic operations: find-set, make-set, and union
+ * makeSet = you're making a circle out of one person
+ * find = find that person's friend circle, return the rep of that circle
+ * union = join two friend circles, everyone becomes friends, rep may or not change
+ *
+ * You have total freedom over how you wanna implement!
+ * That means data structures
+ */
+
+public class DisjointSets {
+
+  // You want some globals or constructor, determined by which improvements you're implementing
+  // using which data structures
+
+  /**
+   * Make a set given a person's name
+   * @param name
+   */
+  public void makeSet(string name) {
+
+  }
+
+  /**
+   * Return the rep of the set the name is part of
+   * @param name person's name
+   * @return representative of that person's set
+   */
+  public string find(string name) {
+
+  }
+
+  /**
+   * Union the set of two people
+   * @param name1 first person's name
+   * @param name2 second person's name
+   */
+  public void union (string name1, string name2) {
+
+  }
+
+  public static void main(String[] args) {
+    string[] names = {"Tasha", "Rui", "Tenzin", "Liz", "Nev", "Loan", "George"};
+    // construct the class
+    // play around!
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+See DisjointSets.java file for more information! Basic idea is to take some time to implement data structures for disjoint sets. Lecture -> action
